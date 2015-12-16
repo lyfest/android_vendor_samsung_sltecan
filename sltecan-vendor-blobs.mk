4941bba01f0d302728f0f3ac7593212c2d9101ef
kyasu <kyasu.m2@gmail.com>
2015-12-16 19:54:26 +0900

    sltecan: Update graphics and drm blobs from hammerhead-mmb29k

--- sltecan-vendor-blobs.mk
+++ sltecan-vendor-blobs.mk
@@ -189,11 +189,11 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/sltecan/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
-    vendor/samsung/sltecan/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    vendor/samsung/sltecan/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/librs_adreno.so:system/vendor/lib/librs_adreno.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/librs_adreno_sha1.so:system/vendor/lib/librs_adreno_sha1.so \
+    vendor/samsung/sltecan/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
+    vendor/samsung/sltecan/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/samsung/sltecan/proprietary/vendor/lib/libscale.so:system/vendor/lib/libscale.so \
     vendor/samsung/sltecan/proprietary/etc/firmware/a330_pfp.fw:system/etc/firmware/a330_pfp.fw \
     vendor/samsung/sltecan/proprietary/etc/firmware/a330_pm4.fw:system/etc/firmware/a330_pm4.fw \
